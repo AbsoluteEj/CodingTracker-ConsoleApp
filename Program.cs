c0dafa866cdba4709df3ed264795dfde766b484a
AbsoluteEj <ejsadiarin@gmail.com>
2022-09-04 16:13:57 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodingTracker_ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Coding Tracker Console App");
+        }
+    }
+}
